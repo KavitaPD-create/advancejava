8f1587face9fee77804d73726b3ddd495da4b724
KavitaPD-create <kavitadaivagnacharya53@gmail.com>
2026-04-18 23:36:11 +0530

    Add files via upload

---
+++ ChecknullorEmpty.java
@@ -0,0 +1,31 @@
+
+/*Q1. Write a Java Program for Checking if a given string is null or contains only 
+whitespace using user defined function isNullOrEmpty().*/
+
+package StringEx;
+import java.util.Scanner;
+
+public class ChecknullorEmpty {
+
+	 
+	public static boolean isNullOrEmpty(String str)
+	{
+	return (str == null || str.trim().isEmpty());
+	}
+
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+
+	System.out.print("Enter a string: ");
+	String input = sc.nextLine();
+
+	if(isNullOrEmpty(input))
+	System.out.println("String is null or empty");
+	else
+	System.out.println("String is not empty");
+
+	sc.close();
+	}
+	}
+
